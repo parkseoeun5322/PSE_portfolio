e9eadfb190f3bd2884cdcb0196ddf279e7bc5cea
seoeun park <78471888+parkseoeun5322@users.noreply.github.com>
2023-02-08 16:20:38 +0900

    이미지 경로 수정

--- src/components/pageUpButton.tsx
+++ src/components/pageUpButton.tsx
@@ -22,7 +22,7 @@ const PageUpButton = () => {
             onClick={scrollToTop}
             style={scrollPosition < 100 ? {display: "none"} : {}} 
         >
-            <img className="pageUpButton__img" src="/src/assets/images/page-up.svg" alt=""></img>    
+            <img className="pageUpButton__img" src={import.meta.env.VITE_IMAGE_URL + 'page-up.svg?raw=true'} alt=""></img>    
         </div>
     )
 }
