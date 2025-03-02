ec88dae7de963dc881b1e6d3597cb47d536e9365
separk <parkseoeun5322@gmail.com>
2025-03-02 18:54:37 +0900

    프로젝트 영역 수정 및 이미지 추가
    1. 스크롤 시 프로젝트 accordion 헤더 고정
    2. accordion 헤더 클릭 시 스크롤 제어
    3. 캐시노트 이미지 변경

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, MouseEvent, useEffect } from 'react';
 import { Accordion } from 'react-bootstrap';
 import projectsData from "../../db/projects.json"
 import projectType from "../../db/projectType.json"
@@ -9,13 +9,64 @@ import ReadMe from './readme';
 const Projects = () => {
     const [show, setShow] = useState(false);
     const [projectData, setProjectData] = useState<Project | null>(null)
+    const [activeHeader, setActiveHeader] = useState<HTMLElement | null>(null);
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const updateScroll = () => {
+        const headerHeight = document.getElementsByClassName('gnb')[0].clientHeight + 1         // 헤더 높이
+        const accordionBtnList = document.getElementsByClassName('accordion-button')            // accordion 헤더 버튼
+        const accordionCollapseList = document.getElementsByClassName('accordion-collapse')     // accordion body
+        const activeAccordion = Array.from(accordionBtnList).find(                              // 활성화 accordion 헤더 버튼
+            (btn) => !btn.classList.contains('collapsed')
+        )
+        const showCollapse = Array.from(accordionCollapseList).find(                            // 활성화 accordion body
+            (item) => item.classList.contains('show')
+        )
+
+        setScrollPosition(window.scrollY || document.documentElement.scrollTop);                // 현재 스크롤 저장
+
+        if(activeAccordion && showCollapse) {
+            let showCollapseTop = showCollapse.getBoundingClientRect().top + window.scrollY - headerHeight
+            let showCollapseBottom = showCollapse.getBoundingClientRect().bottom + window.scrollY - headerHeight - activeAccordion.clientHeight
+
+            if(showCollapseTop < scrollPosition && showCollapseBottom > scrollPosition ) {
+                activeAccordion.closest('.accordion-header')?.classList.add("fixed")
+            } else {
+                activeAccordion.closest('.accordion-header')?.classList.remove("fixed")
+            }
+        }
+        // console.log("scrollPosition:", scrollPosition)
+    }
 
     const handleClick = (data:Project) => () => {
         setShow(true)
         setProjectData(data)
     }
 
+    const headerClick = (event: MouseEvent<HTMLDivElement>) => {
+        const clickedHeader = event.currentTarget      // 클릭한 Header 요소
+        const headerHeight = document.getElementsByClassName('gnb')[0].clientHeight + 1         // 헤더 높이
+        const accordion = document.getElementsByClassName('accordion')[0]
+        const accordionTop = accordion.getBoundingClientRect().top + window.scrollY - headerHeight
+        const headerList = document.getElementsByClassName('accordion-header')
+
+        // fixed된 헤더 제거
+        Array.from(headerList).forEach((header) => { header.classList.remove('fixed') })
+
+        // accordion 영역으로 스크롤 이동
+        setTimeout(() => {
+            window.scrollTo({
+                top: accordionTop,
+                behavior: 'smooth'
+            })
+        }, 300);    // 클래스 제거 후 updateScroll() 실행할 수 있도록 함
+    };
+
     const lastTypeId = (projectType.length - 1).toString()
+
+    useEffect(()=>{
+        window.addEventListener('scroll', updateScroll);
+    });
 
     return (
         <>
@@ -30,7 +81,7 @@ const Projects = () => {
                 <Accordion defaultActiveKey={lastTypeId} flush>
                 {projectType.map((type) => (
                     <Accordion.Item eventKey={type.id} key={type.id}>
-                        <Accordion.Header>
+                        <Accordion.Header onClick={headerClick}>
                             <span className="accordion-button-category">{type.category}</span>
                             { !type.period ? (
                                 <></>
